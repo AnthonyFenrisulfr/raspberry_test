09ebc035bc1dbf5bb4f3b8efdb20b65c2b2e4499
AnthonyFenrisulfr <65879340+AnthonyFenrisulfr@users.noreply.github.com>
2023-03-23 04:37:26 -0600

    Mensajes de sensor DHT11
    Se agrega funcionalidad de envío y recepción de mensajes desde el sensor conectado a la raspberry.

--- subscriber.js
+++ subscriber.js
@@ -1,67 +1,53 @@
-const mqtt = require('mqtt');
-const mongoose = require('mongoose');
+const mqtt = require("mqtt");
+const mongoose = require("mongoose");
 
-// Conexión con el servidor
-const host = 'localhost';
+// Conexión con el servidor MQTT
+const host = "localhost";
 const port = 3000;
 
 const sub = mqtt.connect(`mqtt://${host}:${port}`);
 
-// Conexión con la base de datos
+// Conexión con la base de datos Mongo DB
 const DB = 'prueba';
 const collection = 'datos_sensores';
 const uri = `mongodb://0.0.0.0/${DB}`;
 
 mongoose.connect(uri)
-.then(() => console.log('Connected to database...'))
-.catch((error) => console.log(`Connection to database failed. Error: ${error}`));
+    .then(() => console.log('Connected to database...'))
+    .catch((error) => console.log(`Connection to database failed. Error: ${error}`));
 
 // Definición de la estructura del modelo
 const dbSchema = mongoose.Schema({
-    serieSensor: String,
-    parameters: {
-        temperature: String,
-        humidity: String
+    id_sensor: Number,
+    values: {
+        temperature: Number,
+        humidity: Number
     },
-    date: {
-        day: String,
-        mounth: String,
-        year: String,
-        time: String
-    }
-}, {versionKey: false});
+    time: String,
+    date: String
+}, { versionKey: false });
 
 const dbModel = mongoose.model(collection, dbSchema);
 
 // Suscripción al tópico
-var topic = 'test_topic';
-sub.on('connect', () => {
-    sub.subscribe(topic, (error) => { 
+var topic = "sensor_1/#";
+sub.on("connect", () => {
+    sub.subscribe(topic, (error) => {
         if (!error) {
             console.log(`Subscribed to topic ${topic}`);
         } else {
-            console.log('Subscription error');
+            console.log("Subscription error");
         }
     });
 });
 
-sub.on('message', (topic, message) => {
-    let parametros = message.toString().split('/');
-    create(parametros[0], parametros[1], parametros[2]);
-    console.log(`Sensor número: ${parametros[0]}\nTemperatura: ${parametros[1]}°C\nHumedad: ${parametros[2]}%`);
+sub.on("message", (topic, message) => {
+    console.log(`\nData:\n${message.toString()}\n`);
+    create(message);
+
 });
 
-
 // FUNCIONES ***************************************************************
-
-// Sse obtiene la fecha y hora para el registro
-function getDate() {
-    let actualDate = new Date().toLocaleString();
-    let aux = actualDate.split(' ');
-    let date = aux[0].split('/');
-    date.push(aux[1]);
-    return date;
-}
 
 // Función que devuelve todos los documentos de la base de datos
 async function read() {
@@ -70,26 +56,13 @@ async function read() {
 }
 
 // Función que crea un nuevo registro en la base de datos
-async function create(serie, temp, hum) {
-    let date = getDate();
-    let keys = Object.keys(dbSchema.obj.date);
+async function create(message) {
+    json_msj = JSON.parse(message.toString());
+    data = new dbModel();
 
-    // Se ingresan los parámetros recibidos al modelo
-    let data = new dbModel({
-        serieSensor: serie,
-        parameters: {
-            temperature: temp,
-            humidity: hum
-        }
-    });
-
-    // Se ingresa la fecha al modelo
-    let i = 0;
-    keys.forEach(element => {
-        data.date[element] = date[i];
-        i++;
-    });
-
+    for (const i in json_msj) {
+        data[i] = json_msj[i];
+    }
+    
     await data.save();
 }
-
